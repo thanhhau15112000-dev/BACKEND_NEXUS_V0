c7f19bc6a1aa1372b50022a0ada6d3cdbf1f514f
Le Thanh Hau <thanhhau15112000@gmail.com>
2026-05-12 22:59:01 +0700

    update cors origin for vercel

--- index.js
+++ index.js
@@ -31,7 +31,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://sui-artilleri-v0.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
 
 app.use(express.json({
   limit: '10mb',
